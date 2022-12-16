34128f712d7dfc32368baa618da4994efebe5351
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-16 10:01:35 +0100

    adding conditions for queries

---
+++ LinqTutorial_16dec2022_5/Printer.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqTutorial_16dec2022_5
+{
+    internal static class Printer
+    {
+        public static void Print() 
+        {
+            Console.WriteLine();
+        }
+    }
+}
--- LinqTutorial_16dec2022_5/Program.cs
+++ LinqTutorial_16dec2022_5/Program.cs
@@ -12,5 +12,30 @@ internal class Program
             Console.WriteLine("There's a number larger than 10!");
         }
         else Console.WriteLine("No numbers larger than 10.");
+
+
+        var pets = new[]
+        {
+            new Pet(1, "Hannibal", PetType.Fish, 1.1f),
+            new Pet(2, "Anthony", PetType.Cat, 1.1f),
+            new Pet(3, "Ed", PetType.Cat, 1.1f),
+            new Pet(4, "Taiga", PetType.Dog, 1.1f),
+            new Pet(5, "Rex", PetType.Dog, 1.1f),
+            new Pet(6, "Lucky", PetType.Dog, 1.1f),
+            new Pet(7, "Storm", PetType.Cat, 1.1f),
+            new Pet(8, "Nyan", PetType.Cat, 1.1f)
+        };
+
+        var isAnyPetNamedBruce = pets.Any(pet => pet.Name == "Bruce");
+        Console.WriteLine(isAnyPetNamedBruce);
+
+        var isAnyPetAfish = pets.Any(pet => pet.Type == PetType.Fish);
+        Console.WriteLine(isAnyPetAfish);
+
+        var isThereAVerySpecificPet = pets.Any(pet => pet.Type == PetType.Dog && pet.Name == "Lucky" && pet.Id % 2 == 0 && pet.Name.Length < 6);
+        Console.WriteLine(isThereAVerySpecificPet);
+
+        var isNotEmpty = pets.Any(); /*This returns true if a collection has elements and returns false if the collection is empty.*/
+
     }
 }
