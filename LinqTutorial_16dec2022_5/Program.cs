f44a9c20d5c8044084f41ffb40728d95f7dac13c
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-12-16 09:40:38 +0100

    Add project files.

---
+++ LinqTutorial_16dec2022_5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinqTutorial_16dec2022_5;
+//Any()
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var numbers = new[] { 5, 9, 2, 12, 6 };
+        bool isAnyLargerThan10 = numbers.Any(number => number > 10);
+        if (isAnyLargerThan10 == true)
+        {
+            Console.WriteLine("There's a number larger than 10!");
+        }
+        else Console.WriteLine("No numbers larger than 10.");
+    }
+}
